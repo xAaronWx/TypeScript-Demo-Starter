9008fe7a23d36029755c9db2a6f51f52e9cdef18
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-03-27 09:06:09 -0400

    class

---
+++ app/01-types.ts
@@ -0,0 +1,23 @@
+//standard
+let username: string = "instructor";
+let password: string = "Letmein1234!";
+let instructorId: number = 5;
+
+//value and type without initialization
+let password: string;
+let phoneNumber: number;
+let email: string;
+let socialSecurity: number;
+let isMarried: boolean;
+
+//implicit typing -- the code knows what type this is
+let greeting = "Hello";
+
+//primitive types
+let age: number = 40;
+let currentYear: number = 2021;
+
+//ES6 octal literals
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
